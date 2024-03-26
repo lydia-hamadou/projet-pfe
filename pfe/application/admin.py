0484012393d266bb533a5d6b2ed5df2bbf5200a5
ghania <azzighania2@gmail.comgit config --global user.email azzighania2@gmail.com>
2024-03-26 13:08:06 +0100

    deuxieme

--- pfe/application/admin.py
+++ pfe/application/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Region ,Périmètre ,Prévision_perimetre, Fichier_mansuelle,Utilisateur ,Visualisation,Commentaire
 
-# Register your models here.
+
+admin.site.register(Region)
+admin.site.register(Périmètre)
+admin.site.register(Prévision_perimetre)
+admin.site.register(Fichier_mansuelle)
+admin.site.register(Utilisateur)
+admin.site.register(Commentaire)
+admin.site.register(Visualisation)
+
--- pfe/application/models.py
+++ pfe/application/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Region(models.Model):
+    id_region = models.CharField(max_length=10, primary_key=True)
+    nom = models.CharField(max_length=255)
+
+class Périmètre(models.Model):
+    id_périmètre = models.CharField(max_length=10, primary_key=True)
+    nom = models.CharField(max_length=255)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+
+class Prévision_perimetre(models.Model):
+   
+    id = models.AutoField(primary_key=True)
+    mois = models.IntegerField()
+    annee = models.IntegerField()
+    prévision = models.DecimalField(max_digits=10, decimal_places=2)
+    périmètre = models.ForeignKey(Périmètre, on_delete=models.CASCADE)
+
+class Fichier_mansuelle(models.Model):
+
+    id_fichier = models.AutoField(primary_key=True)
+    mois = models.IntegerField()
+    annee = models.IntegerField()
+    stock_ini = models.DecimalField(max_digits=10, decimal_places=2)
+    Apport_consommation = models.DecimalField(max_digits=10, decimal_places=2)
+    produit = models.DecimalField(max_digits=10, decimal_places=2)
+    consomme = models.DecimalField(max_digits=10, decimal_places=2)
+    preleve = models.DecimalField(max_digits=10, decimal_places=2)
+    pertes = models.DecimalField(max_digits=10, decimal_places=2)
+    expedie = models.DecimalField(max_digits=10, decimal_places=2)
+    laivraison = models.DecimalField(max_digits=10, decimal_places=2)
+    densite = models.DecimalField(max_digits=3, decimal_places=1)
+    périmètre = models.ForeignKey(Périmètre, on_delete=models.CASCADE)
+
+class Utilisateur(models.Model):
+    id_utilisateur = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=255)
+
+class Visualisation(models.Model):
+    id_visualisation = models.AutoField(primary_key=True)
+    date = models.DateField()
+    visualisation = models.CharField(max_length=255)
+
+class Commentaire(models.Model):
+    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    visualisation = models.ForeignKey(Visualisation, on_delete=models.CASCADE)
+    date = models.DateField()
+    commentaire = models.TextField()
+    class Meta:
+        unique_together = (('utilisateur', 'visualisation'),)
+
+
+
